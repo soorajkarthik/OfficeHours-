d700274f0a5e3966e0f26f9c3296c428dfdb77bf
Rajavelu, Rithvik Krithin <rithvik.rajavelu@gmail.com>
2021-10-23 13:04:22 -0400

    Sidebar
    sidebar update

--- frontend/ohp-react/src/App.js
+++ frontend/ohp-react/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Sidebar />
 
-      </header>
     </div>
   );
 }
---
+++ frontend/ohp-react/src/Components/SidebarData.js
@@ -0,0 +1,30 @@
+import React from "react";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ComputerIcon from '@mui/icons-material/Computer';
+
+
+export const SidebarData = [
+    {
+        title: "New Channel",
+        icon: <AddCircleIcon />,
+        link: "/home"
+    },
+
+    {
+        title: "CS 1332",
+        icon: <ComputerIcon />,
+        link: "/home"
+    },
+
+    {
+        title: "CS 2110",
+        icon: <ComputerIcon />,
+        link: "/home"
+    },
+
+    {
+        title: "CS 3600",
+        icon: <ComputerIcon />,
+        link: "/home"
+    }
+]
